e9fd7ec3120ee943aa46f0a6b22c0c1020d9bd30
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-06-15 14:45:09 +0300

    remove menu mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,13 @@ if(navclose){
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 
