bc4207636dedbca2b93806c8bad21f8eaeaf7ad9
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-06-15 21:37:48 +0300

    change background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,11 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SWIPER DISCOVER ====================*/
 
