a424ad8353c452ae6f012363bea8dd0169ecab63
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-06-19 10:47:11 +0300

    discover section style and swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,17 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*==================== SWIPER DISCOVER ====================*/
-
+let swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+})
 
 /*==================== VIDEO ====================*/
 
